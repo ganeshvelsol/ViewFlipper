40054a728de1943b0f8011ec97bea05b6e5163d8
ganeshvelsol <ganesh.velsol@gmail.com>
2018-11-13 12:16:49 +0530

    viewflipper on clicklistenr

--- app/src/main/java/com/example/velsol/myapplication/ViewFlipperExample.java
+++ app/src/main/java/com/example/velsol/myapplication/ViewFlipperExample.java
@@ -2,12 +2,15 @@ package com.example.velsol.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 public class ViewFlipperExample extends AppCompatActivity
 {
     ViewFlipper flips;
+    int i;
     int img[]={R.drawable.food,R.drawable.fish,R.drawable.french_fries,R.drawable.pizza};
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,19 +19,38 @@ public class ViewFlipperExample extends AppCompatActivity
         setContentView(R.layout.activity_view_flipper_example);
         flips=(ViewFlipper)findViewById(R.id.flips);
 
-        for (int image : img)
+        for (i=0;i<img.length;i++)
         {
-            flipperImage(image);
+            flipperImage(img[i]);
         }
+
+
     }
-    public void flipperImage(int image)
+    public void flipperImage(final int image)
     {
         ImageView im=new ImageView(this);
         im.setBackgroundResource(image);
+        im.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                switch (image)
+                {
+                    case R.drawable.food :
+                    {
+                        Toast.makeText(ViewFlipperExample.this, "food", Toast.LENGTH_SHORT).show();
+                    }
+                    default:
+                        Toast.makeText(ViewFlipperExample.this, ""+image, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
         flips.addView(im);
         flips.setFlipInterval(4000);
         flips.setAutoStart(true);
-        flips.setInAnimation(this,android.R.anim.slide_out_right);
-        flips.setOutAnimation(this,android.R.anim.slide_in_left);
+//        flips.setInAnimation(this,android.R.anim.slide_out_right);
+//        flips.setOutAnimation(this,android.R.anim.slide_in_left);
     }
 }
